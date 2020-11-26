575aa736cb488409426ccf5a77e07130dce61c22
Joseph <nguld@itsj-group.com>
2020-11-26 11:52:44 +0700

    added Eureka Client

--- spring-microservice-joseph/src/main/java/com/example/joseph/springmicroservicejoseph/controller/PurchaseController.java
+++ spring-microservice-joseph/src/main/java/com/example/joseph/springmicroservicejoseph/controller/PurchaseController.java
@@ -7,9 +7,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(value = "/purchase")
+@RestController
+@RequestMapping(value = "/purchase")
 public class PurchaseController {
 
   private static final Logger logger = LoggerFactory.getLogger(PurchaseController.class);
