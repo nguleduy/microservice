fa3fef0b7f290aa0f5530bc3eb822c8f56745dea
Joseph <nguld@itsj-group.com>
2020-11-26 11:40:05 +0700

    added RestTemplate

--- spring-microservice-joseph/src/main/java/com/example/joseph/springmicroservicejoseph/controller/PurchaseController.java
+++ spring-microservice-joseph/src/main/java/com/example/joseph/springmicroservicejoseph/controller/PurchaseController.java
@@ -1,8 +1,10 @@
 package com.example.joseph.springmicroservicejoseph.controller;
 
 import com.example.joseph.springmicroservicejoseph.dto.PurchaseDTO;
+import com.example.joseph.springmicroservicejoseph.service.IPurchaseService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +14,13 @@ public class PurchaseController {
 
   private static final Logger logger = LoggerFactory.getLogger(PurchaseController.class);
 
+  @Autowired
+  private IPurchaseService iPurchaseService;
+
   @PostMapping
-  public void makePurchase(@RequestBody PurchaseDTO purchase) {
+  public void makePurchase(@RequestBody PurchaseDTO purchaseDTO) {
     logger.info("Make Purchase");
+    this.iPurchaseService.makePurchase(purchaseDTO);
   }
 
 }
