7ed536735670defb3c835f08fcd32acd03395520
Joseph <nguld@itsj-group.com>
2020-11-26 10:27:26 +0700

    init spring-microservice-joseph

---
+++ spring-microservice-joseph/src/main/java/com/example/joseph/springmicroservicejoseph/SpringMicroserviceJosephApplication.java
@@ -0,0 +1,13 @@
+package com.example.joseph.springmicroservicejoseph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroserviceJosephApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroserviceJosephApplication.class, args);
+	}
+
+}
---
+++ spring-microservice-joseph/src/test/java/com/example/joseph/springmicroservicejoseph/SpringMicroserviceJosephApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.joseph.springmicroservicejoseph;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroserviceJosephApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
