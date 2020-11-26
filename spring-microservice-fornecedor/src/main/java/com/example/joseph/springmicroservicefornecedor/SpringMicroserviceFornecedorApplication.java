44680fcd0d7b571937515acdd419eb36b98725aa
Joseph <nguld@itsj-group.com>
2020-11-26 16:18:39 +0700

    init spring-microservice-fornecedor

---
+++ spring-microservice-fornecedor/src/main/java/com/example/joseph/springmicroservicefornecedor/SpringMicroserviceFornecedorApplication.java
@@ -0,0 +1,13 @@
+package com.example.joseph.springmicroservicefornecedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroserviceFornecedorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroserviceFornecedorApplication.class, args);
+	}
+
+}
