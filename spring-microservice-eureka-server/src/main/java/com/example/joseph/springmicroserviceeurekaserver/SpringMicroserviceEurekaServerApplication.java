41d563587c6f82ba287cad23850b2cf0499992ff
Joseph <nguld@itsj-group.com>
2020-11-27 11:20:52 +0700

    init spring-microservice-eureka-server

---
+++ spring-microservice-eureka-server/src/main/java/com/example/joseph/springmicroserviceeurekaserver/SpringMicroserviceEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.joseph.springmicroserviceeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroserviceEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroserviceEurekaServerApplication.class, args);
+	}
+
+}
---
+++ spring-microservice-eureka-server/src/test/java/com/example/joseph/springmicroserviceeurekaserver/SpringMicroserviceEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.joseph.springmicroserviceeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroserviceEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
