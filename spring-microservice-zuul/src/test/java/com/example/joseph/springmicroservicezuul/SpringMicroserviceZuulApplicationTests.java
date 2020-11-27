b8b27776ca13679f66c47a9b2d7229e861ff0dfe
Joseph <nguld@itsj-group.com>
2020-11-27 15:33:27 +0700

    init spring-microservice-zuul

---
+++ spring-microservice-zuul/src/test/java/com/example/joseph/springmicroservicezuul/SpringMicroserviceZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.joseph.springmicroservicezuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroserviceZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
