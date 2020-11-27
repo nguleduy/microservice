f8e141352e04a825364ed149738c4cc8620b1f4c
Joseph <nguld@itsj-group.com>
2020-11-27 15:12:34 +0700

    init spring-microservice-transportation

---
+++ spring-microservice-transportation/src/main/java/com/example/joseph/springmicroservicetransportation/SpringMicroserviceTransportationApplication.java
@@ -0,0 +1,13 @@
+package com.example.joseph.springmicroservicetransportation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroserviceTransportationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroserviceTransportationApplication.class, args);
+	}
+
+}
---
+++ spring-microservice-transportation/src/test/java/com/example/joseph/springmicroservicetransportation/SpringMicroserviceTransportationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.joseph.springmicroservicetransportation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroserviceTransportationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
