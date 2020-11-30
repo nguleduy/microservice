8725287a2be115eee83949748778d22dd2101def
Joseph <nguld@itsj-group.com>
2020-11-30 16:21:19 +0700

    updated spring-microservice-boot-admin

--- spring-microservice-boot-admin/src/main/java/com/example/joseph/springmicroservicebootadmin/SpringMicroserviceBootAdminApplication.java
+++ spring-microservice-boot-admin/src/main/java/com/example/joseph/springmicroservicebootadmin/SpringMicroserviceBootAdminApplication.java
@@ -1,13 +1,15 @@
 package com.example.joseph.springmicroservicebootadmin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class SpringMicroserviceBootAdminApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringMicroserviceBootAdminApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringMicroserviceBootAdminApplication.class, args);
+  }
 
 }
