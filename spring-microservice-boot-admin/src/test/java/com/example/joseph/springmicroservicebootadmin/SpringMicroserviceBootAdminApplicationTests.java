55f273c91442df6ee05636435a881e8672e6bd47
Joseph <nguld@itsj-group.com>
2020-11-30 16:18:40 +0700

    init spring-microservice-boot-admin

---
+++ spring-microservice-boot-admin/src/test/java/com/example/joseph/springmicroservicebootadmin/SpringMicroserviceBootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.joseph.springmicroservicebootadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroserviceBootAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
