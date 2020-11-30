028619ee6596333c90d09d6779c8230bdcfdfb1a
Joseph <nguld@itsj-group.com>
2020-11-30 16:11:41 +0700

    added Configs Default for OAuth2 + Security. Generate Token

--- spring-microservice-auth/src/main/java/com/example/joseph/springmicroserviceauth/SpringMicroserviceAuthApplication.java
+++ spring-microservice-auth/src/main/java/com/example/joseph/springmicroserviceauth/SpringMicroserviceAuthApplication.java
@@ -2,12 +2,26 @@ package com.example.joseph.springmicroserviceauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
+@EnableAuthorizationServer
+@EnableResourceServer
+@RestController
 public class SpringMicroserviceAuthApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringMicroserviceAuthApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringMicroserviceAuthApplication.class, args);
+  }
+
+  @GetMapping("/user")
+  public Principal user(Principal user) {
+    return user;
+  }
 
 }
