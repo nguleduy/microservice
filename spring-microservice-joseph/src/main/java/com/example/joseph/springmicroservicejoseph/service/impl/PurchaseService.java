e3bd97199245837e9a84c27284eb102bd9ca0760
Joseph <nguld@itsj-group.com>
2020-11-30 11:03:08 +0700

    added HystrixCommand for fallback and circuit breaker

--- spring-microservice-joseph/src/main/java/com/example/joseph/springmicroservicejoseph/service/impl/PurchaseService.java
+++ spring-microservice-joseph/src/main/java/com/example/joseph/springmicroservicejoseph/service/impl/PurchaseService.java
@@ -7,6 +7,7 @@ import com.example.joseph.springmicroservicejoseph.dto.InfoProviderDTO;
 import com.example.joseph.springmicroservicejoseph.dto.PurchaseDTO;
 import com.example.joseph.springmicroservicejoseph.model.Purchase;
 import com.example.joseph.springmicroservicejoseph.service.IPurchaseService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,7 @@ public class PurchaseService implements IPurchaseService {
   private ProviderClient providerClient;
 
   @Override
+  @HystrixCommand
   public Purchase makePurchase(PurchaseDTO purchaseDTO) {
     final String state = purchaseDTO.getAddress().getState();
 
