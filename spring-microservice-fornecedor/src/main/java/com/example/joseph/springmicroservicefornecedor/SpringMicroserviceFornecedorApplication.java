49d2eb4f313fb0aae5e438c6aab9955079fe4dbd
Joseph <nguld@itsj-group.com>
2020-12-01 11:28:12 +0700

    added Spring Boot Admin; Swagger; Actuator;

--- spring-microservice-fornecedor/src/main/java/com/example/joseph/springmicroservicefornecedor/SpringMicroserviceFornecedorApplication.java
+++ spring-microservice-fornecedor/src/main/java/com/example/joseph/springmicroservicefornecedor/SpringMicroserviceFornecedorApplication.java
@@ -3,9 +3,11 @@ package com.example.joseph.springmicroservicefornecedor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableResourceServer
+@EnableSwagger2
 public class SpringMicroserviceFornecedorApplication {
 
 	public static void main(String[] args) {
---
+++ spring-microservice-fornecedor/src/main/java/com/example/joseph/springmicroservicefornecedor/auth/SwaggerConfiguration.java
@@ -0,0 +1,52 @@
+package com.example.joseph.springmicroservicefornecedor.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+
+@Configuration
+public class SwaggerConfiguration {
+
+  @Bean
+  public Docket forumApi() {
+    return new Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("com.example.joseph.microservicefornecedor.controller"))
+            .paths(PathSelectors.any())
+            .build()
+            .ignoredParameterTypes(disableTemplateClassesModels())
+            .apiInfo(apiInfo());
+  }
+
+  private ApiInfo apiInfo() {
+    return new ApiInfoBuilder()
+            .title("Spring Microservice - Fornecedor API")
+            .description("Microservices Project Using: Spring Boot + Eureka (registry/discovery) + Spring Cloud (config server) "
+                    + "+ Feign (Clientside LoadBalance) + Logs and distributed tracing with Papertrail "
+                    + "+ Sleuth + Hystrix (Fallback and Circuit Breaker) + API Gateway + Spring Zuul "
+                    + "+ Authentication and Authorization between microservices using OAuth2.")
+            .contact(new Contact("Joseph", "https://github.com/nguleduy/", "nguleduy@gmail.com"))
+            .license("License 1.0.0")
+            .licenseUrl("https://github.com/nguleduy/microservice")
+            .version("1.0.0")
+            .build();
+  }
+
+  //Method that returns templates that will be hidden in the API documentation
+  @SuppressWarnings("rawtypes")
+  private Class[] disableTemplateClassesModels() {
+    ArrayList<Class> classForDisable = new ArrayList<Class>();
+
+    //Entities - Models - DTO and others...
+
+    return classForDisable.toArray(new Class[classForDisable.size()]);
+  }
+}
