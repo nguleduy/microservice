79a58afc3dcc72f6320767d3175c2fd6ce61fc5d
Joseph <nguld@itsj-group.com>
2020-12-01 11:22:16 +0700

    added Config for communication Fornecedor Authentication with Server Authentication; token; oauth2

--- spring-microservice-fornecedor/src/main/java/com/example/joseph/springmicroservicefornecedor/SpringMicroserviceFornecedorApplication.java
+++ spring-microservice-fornecedor/src/main/java/com/example/joseph/springmicroservicefornecedor/SpringMicroserviceFornecedorApplication.java
@@ -2,8 +2,10 @@ package com.example.joseph.springmicroservicefornecedor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
 public class SpringMicroserviceFornecedorApplication {
 
 	public static void main(String[] args) {
